f9fbdbdcce897df564e4899b27e066be8da79d03
mcortex <cortexxx.bomb@gmail.com>
2017-09-08 07:11:30 +0300

    Test de GPIO - Commit inicial

---
+++ GPIO-tests/test.py
@@ -0,0 +1,9 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)
+
+p = GPIO.PWM(12, 0.5)
+p.start(1)
+input('Presiona Enter para parar:')
+p.stop()
+GPIO.cleanup()
---
+++ hello_world.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+# blinking function  
+def blink(pin):  
+    GPIO.output(pin,GPIO.HIGH)  
+    time.sleep(1)  
+    GPIO.output(pin,GPIO.LOW)  
+    time.sleep(1)  
+    return
+
+# to use Raspberry Pi board pin numbers  
+GPIO.setmode(GPIO.BCM)
+
+# set up GPIO output channel  
+GPIO.setup(4, GPIO.OUT)
+
+# blink GPIO17 50 times  
+for i in range(0,50):  
+    blink(4)
+GPIO.cleanup()
